c2e6aeb834f601f6274e55e2aa925d8b0045b7ff
Uropb1221 <94992413+Uropb1221@users.noreply.github.com>
2022-03-23 19:22:38 +0300

    gg

---
+++ ConsoleAppHomework/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Homework
+{
+    class Delegate
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            Console.WriteLine("Привет, напиши любой символ)");
+            var a = Console.ReadKey();
+            while (true)
+            { 
+                if (a.Key == ConsoleKey.C) 
+                {
+                    Console.WriteLine("Завершаем выполнение цикла");
+                    break;//завершаем выполнение цикла
+                }
+                else
+                {
+                 OnKeyPressed?.Invoke(this, a.KeyChar);//Вызов события OnKeyPressed
+                } 
+            }
+        }
+    }
+
+    class Work 
+    {
+        static void Main(string[] args)
+        {
+            var b = new Work();
+            b.Add();
+        }
+        public void Write(object sender, char letter)
+        {
+            Console.WriteLine(letter);
+        }
+        public void Add()
+        {
+            var c = new Delegate();
+
+            c.OnKeyPressed += Write;
+            c.Run();
+        }
+
+    }
+}
